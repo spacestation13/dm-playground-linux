988bb8e07367a6301da1f0a33868f11f9c821fda
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-12-25 01:44:09 -0500

    Initial Commit

---
+++ external.mk
@@ -0,0 +1,2 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_BYONDVM_PATH)/package/*/*.mk))
+
---
+++ package/byondvm-controller/byondvm-controller.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# byondvm-controller
+#
+################################################################################
+
+BYONDVM_CONTROLLER_VERSION = f1f0d56de65d2f73b408761dc618eae4c065bcbb
+BYONDVM_CONTROLLER_SITE = $(call github,spacestation13,dm-playground_controller,$(BYONDVM_CONTROLLER_VERSION))
+BYONDVM_CONTROLLER_LICENSE = ISC
+
+BYONDVM_CONTROLLER_DEPENDENCIES = host-rustc
+BYONDVM_CONTROLLER_CARGO_ENV = \
+	CARGO_HOME=$(HOST_DIR)/share/cargo \
+	LIBRARY_PATH=$(HOST_DIR)/usr/lib
+
+BYONDVM_CONTROLLER_CARGO_OPTS = \
+	--release \
+	--manifest-path=$(@D)/Cargo.toml \
+	--target=$(RUSTC_TARGET_NAME)
+
+define BYONDVM_CONTROLLER_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(BYONDVM_CONTROLLER_CARGO_ENV) \
+		cargo build $(BYONDVM_CONTROLLER_CARGO_OPTS)
+endef
+
+define BYONDVM_CONTROLLER_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/dm-playground_controller $(TARGET_DIR)/usr/bin/controller
+endef
+
+$(eval $(generic-package))
