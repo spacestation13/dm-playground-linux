d50c4aaf10f65539cb6e6968ffee201638eaf2eb
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2023-06-22 21:19:28 -0400

    Fixes build apparently

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 cd ../buildroot || exit
 core_count=$(grep -c   "^processor" /proc/cpuinfo)
 make -j "$core_count"
+make -j "$core_count"
--- setup.sh
+++ setup.sh
@@ -11,13 +11,13 @@ mkdir temp
 wget https://buildroot.org/downloads/buildroot-2022.05.1.tar.gz -O temp/buildroot.tar.gz
 tar -xvf temp/buildroot.tar.gz
 rm temp/buildroot.tar.gz
-mv temp/buildroot-* ../buildroot
+mv buildroot-* ../buildroot
 rmdir temp
 
 #Setup symlink
-ln -s ../byondvm ../buildroot/byondvm
+ln -s "$(pwd)" ../buildroot/byondvm
 
 #Load default config and do initial build
 cd ../buildroot || exit
 make defconfig byondvm_defconfig BR2_EXTERNAL=byondvm
-./build.sh
+./byondvm/build.sh
